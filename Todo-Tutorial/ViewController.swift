6f1e23e6c45232ddd97ee363e735375b08289a8d
Mitchell Hudson <soggybag@gmail.com>
2015-03-27 14:09:22 -0700

    Initial Commit

---
+++ Todo-Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo-Tutorial
+//
+//  Created by mitchell hudson on 3/27/15.
+//  Copyright (c) 2015 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Todo-TutorialTests/Todo_TutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  Todo_TutorialTests.swift
+//  Todo-TutorialTests
+//
+//  Created by mitchell hudson on 3/27/15.
+//  Copyright (c) 2015 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Todo_TutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
